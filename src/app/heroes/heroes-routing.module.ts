3bdaf7b2872dc79a7645630b2b0882cdc171667c
fcogallegos <fgallegosgo@gmail.com>
2022-01-20 12:38:14 -0700

    homework about child routes

---
+++ src/app/heroes/heroes-routing.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AddComponent } from './pages/add/add.component';
+import { ListComponent } from './pages/list/list.component';
+import { SearchComponent } from './pages/search/search.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+
+
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'list',
+        component: ListComponent
+      },
+      {
+        path: 'add',
+        component: AddComponent
+      },
+      {
+        path: 'edit/:id',
+        component: AddComponent
+      },
+      {
+        path: 'search',
+        component: SearchComponent
+      },
+      {
+        path: 'id',
+        component: HeroeComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'list'
+      }
+    ]
+  }
+]
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild( routes )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class HeroesRoutingModule { }
