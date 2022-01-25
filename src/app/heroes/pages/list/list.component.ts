7559831b271b1b58124d0682e80233d2e8cb780a
fcogallegos <fgallegosgo@gmail.com>
2022-01-25 09:58:14 -0700

    Flex Layout - differents resolutions

--- src/app/heroes/pages/list/list.component.ts
+++ src/app/heroes/pages/list/list.component.ts
@@ -5,7 +5,11 @@ import { Heroe } from '../heroe/interfaces/heroe.interface';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styles: [
+  styles: [`
+    mat-card {
+      margin-top: 20px
+    }
+  `
   ]
 })
 export class ListComponent implements OnInit {
